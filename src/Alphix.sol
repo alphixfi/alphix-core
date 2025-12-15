c40af666e45ed1f55357d6c88a24668e7d49234b
yanisepfl <yanis.berkani@epfl.ch>
2025-12-15 21:07:10 +0530

    Sherlock Audit - Issue 14: _setLogic now also updates registry stored logic value

--- src/Alphix.sol
+++ src/Alphix.sol
@@ -93,7 +93,6 @@ contract Alphix is
             revert InvalidAddress();
         }
         _setLogic(_logic);
-        IRegistry(registry).registerContract(IRegistry.ContractKey.AlphixLogic, _logic);
         _unpause();
     }
 
@@ -409,6 +408,7 @@ contract Alphix is
         }
         emit LogicUpdated(logic, newLogic);
         logic = newLogic;
+        IRegistry(registry).registerContract(IRegistry.ContractKey.AlphixLogic, newLogic);
     }
 
     /**
